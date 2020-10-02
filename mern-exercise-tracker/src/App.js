e105b042bc03b50f29eda7699aae34e59e6ce00c
Swati_Wuppuluri <wswati.sdw@gmail.com>
2020-10-02 17:25:58 +0530

    code changes

--- mern-exercise-tracker/src/App.js
+++ mern-exercise-tracker/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import ExercisesList from "./components/ExercisesList";
