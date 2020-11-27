e8c432af8f2ed2ebc74f61061f74e4c2f205145b
Swati_Wuppuluri <wswati.sdw@gmail.com>
2020-11-27 11:43:06 +0530

    trial revert

--- mern-exercise-tracker/backend/routes/exercises.js
+++ mern-exercise-tracker/backend/routes/exercises.js
@@ -4,7 +4,7 @@ let Exercise = require('../models/exercise.model');
 router.route('/').get((req,res) => {
     Exercise.find()
         .then(exercises => res.json(exercises))
-        .catch(err => res.status(400).json('Error: ' +errs));
+        .catch(err => res.status(400).json('Error: ' +err));
 });
 
 router.route('/add').post((req,res) => {
